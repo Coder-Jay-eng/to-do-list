1554f09aa50fd12388d5d75945eacdcff7004e4e
Aw3som3ss <jabwunza@gmail.com>
2023-06-27 15:06:50 +0300

    Add, do activity

--- src/components/CreateToDo.jsx
+++ src/components/CreateToDo.jsx
@@ -1,7 +1,46 @@
-import React from 'react';
+import React,{useState} from 'react';
 import ToDoList from './ToDoList';
+import swal from 'sweetalert';
 
 function CreateToDo() {
+
+    const [todo, setTodo] = useState({title: "", done: false})
+    const [todoArr, setTodoArr] = useState([])
+
+    let todos = localStorage.hasOwnProperty("todos") 
+    ? JSON.parse(localStorage.getItem("todos")) 
+    : []
+
+    const onChange = (event) => {
+        let {value} = event.target
+        let obj = {}
+            obj["title"] = value
+            obj["done"] = false
+            setTodo(obj)
+    }
+
+    const CreateTodo = (event) => {
+        const {name} = event.target
+        if(event.key === "Enter" || name === "addTodo") {
+            if(todo.title !== "") {
+                todos.unshift(todo)
+                localStorage.setItem("todos", JSON.stringify(todos))
+                setTodo({title: "", done: false})
+            } else {
+                swal("Kindly write activity first", "error")
+            }
+        }
+    }
+
+    const completeTodo = (i) => {
+        if(todos[i]["done"] !== true){
+            todos[i]["done"] = true;
+            localStorage.setItem("todos", JSON.stringify(todos))
+            setTodoArr(todos)
+            swal("Awesome!", "Todo Completed", "success");
+        }
+    }
+
   return (
     <>
     <div className='box'>
@@ -10,12 +49,14 @@ function CreateToDo() {
             <h4>What do you want to do?</h4>
         </div>
         <div className='text-addToDo'>
-            <input type='text' name='todo' placeholder='Add activity...'/>
-            <button className='btn-addToDo' type='button' name='addToDo'>Add ToDo</button>
+            <input type='text' name='todo' placeholder='Add activity...' 
+            value={todo.title} onKeyPress={CreateTodo} onChange={onChange}/>
+            <button className='btn-addToDo' type='button' name='addToDo' onClick={CreateTodo}>Add ToDo</button>
         </div>
         </div>
     
-    <ToDoList/>
+    <ToDoList todoArr = {todoArr}
+    completeTodo={completeTodo}/>
     </>
   )
 }
--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,40 +1,30 @@
 import React from 'react'
 
-function ToDoList() {
+function ToDoList(props) {
+   const {completeTodo} = props
+    let todoArr = props.todoArr.length > 0 
+    ? props.todoArr : JSON.parse(localStorage.getItem('todos'))
+
+
   return (
     <div className='todo-list'>
         <ul>
-            <li>
-                <div>Shopping</div>
+            {todoArr && todoArr.length > 0 ?
+        todoArr.map((el, i) => (
+            <li key={i}>
+                <div className={el["done"] ? "line-through" : null}>{el.title}</div>
                 <div className='icon'>
-                    <i title='Complete' className='fas fa-check circle pointer'/>
+                    <i title='Complete' onClick={()=>completeTodo(i)} className={`fas fa-check-circle pointer ${el["done"] ? "green" : "blue"}`}/>
                     <i title='Delete' className='fas fa-trash-alt pointer'/>
                 </div>
             </li>
-            <li>
-                <div>Play</div>
-                <div className='icon'>
-                    <i title='Complete' className='fas fa-check circle pointer'/>
-                    <i title='Delete' className='fas fa-trash-alt pointer'/>
-                </div>
-            </li>
-            <li>
-                <div>Sleep</div>
-                <div className='icon'>
-                    <i title='Complete' className='fas fa-check circle pointer'/>
-                    <i title='Delete' className='fas fa-trash-alt pointer'/>
-                </div>
-            </li>
-            <li>
-                <div>Work out</div>
-                <div className='icon'>
-                    <i title='Complete' className='fas fa-check circle pointer'/>
-                    <i title='Delete' className='fas fa-trash-alt pointer'/>
-                </div>
-            </li>
+        )) : null
+        }
+            
         </ul>
     </div>
+    
   );
 }
 
-export default ToDoList
+export default ToDoList;
